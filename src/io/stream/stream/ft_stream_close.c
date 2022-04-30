bcb0d8be0b9e84ae89f9a539f285d000c4032b26
bgenia <bgenia@student.21-school.ru>
2022-04-30 13:58:21 +0300

    Make ft_stream_close mark the stream as invalid

--- src/io/stream/stream/ft_stream_close.c
+++ src/io/stream/stream/ft_stream_close.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 10:41:38 by bgenia            #+#    #+#             */
-/*   Updated: 2022/04/15 20:02:19 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/04/30 12:48:40 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,5 @@ void
 		ft_stream_buffer_destroy(&stream->_read_buffer);
 	if (ft_stream_is_closable(stream))
 		stream->_hclose(&stream->_context);
+	stream->is_valid = false;
 }
