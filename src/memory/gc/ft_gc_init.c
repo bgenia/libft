74b8713a34397089c9e418fdf1781176e6726f0d
bgenia <bgenia@student.21-school.ru>
2022-05-07 15:09:29 +0300

    Make ft_fc_init return immediately if ft_vector_alloc fails

--- src/memory/gc/ft_gc_init.c
+++ src/memory/gc/ft_gc_init.c
@@ -6,7 +6,7 @@
 /*   By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 17:38:24 by bgenia            #+#    #+#             */
-/*   Updated: 2022/05/07 13:20:49 by bgenia           ###   ########.fr       */
+/*   Updated: 2022/05/07 14:23:41 by bgenia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@ bool
 	ft_gc_init_with_capacity(t_gc *gc, size_t capacity, bool fixed)
 {
 	gc->vec_records = ft_vector_alloc_with_capacity(sizeof(*gc->vec_records), capacity);
+	if (!gc->vec_records)
+		return (false);
 	if (fixed)
 		ft_vector_set_flags(gc->vec_records, 0);
 	else
 		ft_vector_set_flags(gc->vec_records, FT_VECTOR_GROWABLE);
-	return (!!gc->vec_records);
+	return (true);
 }
 
 bool
